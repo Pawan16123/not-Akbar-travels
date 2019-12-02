65f7587d6c59a07729b500051fcd9ba1fe608bed
Pawan <poojary.pawan16@gmail.com>
2019-12-02 18:25:00 +0530

    webpack part1 done

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+module.exports = {
+    entry:"./app/assets/scripts/App.js",
+    output: {
+        filename: "bundeled.js",
+        path: path.resolve(__dirname,"app")
+    },
+    mode:"development",
+    watch: true
+}
