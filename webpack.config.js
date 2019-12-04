96b82c3992066201eb2f7a5ed32cc83d3b7fb9c8
Pawan <poojary.pawan16@gmail.com>
2019-12-04 17:06:34 +0530

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,14 @@ module.exports = {
         path: path.resolve(__dirname,"app"),
     },
     devServer: {
+        before: function(app,server){
+            server._watch("./app/**/*.html")
+        },
         contentBase: path.join(__dirname,"app"),
         hot: true,  //allow webpack new css or js after saving i.e on the fly hot module replacementort files order didn't worked for me.
         port: 3000, //default value 8080
+        host: "0.0.0.0"
+
     },
     mode:"development",
     module: {
